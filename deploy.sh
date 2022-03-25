83f109900f3296f702b091536d0fe036429dc4d9
Dominic Soh <dominic.soh@amptalk.co.jp>
2022-03-25 13:21:32 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:dominic-soh/mini-web-page master:gh-pages
+git push -f git@github.com:dominic-soh/example-about-page master:gh-pages
 cd -
